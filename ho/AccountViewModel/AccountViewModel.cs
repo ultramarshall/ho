ebb144dde17674d28129a1bf037796355187a8c3
Marshall <Marshall@DESKTOP-3RKCN07>
2017-03-12 23:01:48 +0700

    first commit

---
+++ ho/AccountViewModel/AccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ho.Models;
+
+namespace ho.AccountViewModel
+{
+    public class AccountViewModel
+    {
+        public AccountViewModel()
+        {
+
+        }
+    }
+}
---
+++ ho/Models/AccountModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ho.Models
+{
+    public class AccountModel
+    {
+        private List<Account> List = new List<Account>();
+
+        public AccountModel()
+        {
+            List.Add(new Account { Username = "acc1", Password = "123", Roles = "1"});
+            List.Add(new Account { Username = "acc2", Password = "123", Roles = "1"});
+            List.Add(new Account { Username = "acc3", Password = "123", Roles = "1" });
+        }
+
+    }
+}

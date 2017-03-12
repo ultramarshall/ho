820d86f0aee742ad8bd6d30923168a770516b25a
Marshall <Marshall@DESKTOP-3RKCN07>
2017-03-12 22:50:05 +0700

    Add project files.

---
+++ ho/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ho.Startup))]
+namespace ho
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
